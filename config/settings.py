dbea8f92b0defc77219dc270b183eb9b27a03779
Jaotiana Donaldo ZOUBERY <125810098+donaldozoubery@users.noreply.github.com>
2025-01-02 18:41:18 +0100

    settings.py
    serveur smtp

--- config/settings.py
+++ config/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # For Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.mail.me.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
