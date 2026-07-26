129fb561bd010e9f9e221b80dd7a3720a4e5915e
donaldozoubery <dzoubery@gmail.com>
2026-07-26 12:51:09 +0300

    Fix media on Vercel: use Cloudinary as default file storage

--- config/settings.py
+++ config/settings.py
@@ -169,6 +169,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
