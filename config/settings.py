a32df0b25f685a44b3776eaf722164f1650b6100
donaldozoubery <dzoubery@gmail.com>
2026-07-27 22:08:12 +0300

    Fix 400 on Vercel: remove secure cookie flags behind proxy

--- config/settings.py
+++ config/settings.py
@@ -35,8 +35,6 @@ if not DEBUG:
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
     X_FRAME_OPTIONS = 'DENY'
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
